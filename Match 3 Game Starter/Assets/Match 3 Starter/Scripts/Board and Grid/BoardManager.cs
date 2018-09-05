4b3d9f72e1995d2092a6e7ddc7e2215261ab8641
Richadavy <richadavy763543@my.yosemite.edu>
2018-09-05 15:43:07 -0700

    Finished Match 3 Tutorial

---
+++ Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -0,0 +1,163 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+	public static BoardManager instance;
+	public List<Sprite> characters = new List<Sprite>();
+	public GameObject tile;
+	public int xSize, ySize;
+
+	private GameObject[,] tiles;
+
+	public bool IsShifting { get; set; }
+
+	void Start () {
+		instance = GetComponent<BoardManager>();
+
+		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+	private void CreateBoard (float xOffset, float yOffset) {
+		tiles = new GameObject[xSize, ySize];
+
+        float startX = transform.position.x;
+		float startY = transform.position.y;
+
+        //Sprite variables to disable auto comboes at start
+        Sprite[] previousLeft = new Sprite[ySize];
+        Sprite previousBelow = null;
+
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+				tiles[x, y] = newTile;
+
+                //Randomizes board
+                newTile.transform.parent = transform;
+
+                //algorithm to stop auto combo at start
+                List<Sprite> possibleCharacters = new List<Sprite>();
+                possibleCharacters.AddRange(characters);
+                possibleCharacters.Remove(previousLeft[y]);
+                possibleCharacters.Remove(previousBelow);
+
+                Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                //Changes last Sprite to next one
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
+			}
+        }
+    }
+
+    //goes through whole board to identify empty spaces after matches
+    public IEnumerator FIndNullTiles()
+    {
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                if (tiles[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(ShiftTilesDown(x, y));
+                    break;
+                }
+            }
+        }
+
+        //finds number of made combos
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                tiles[x, y].GetComponent<Tile>().ClearAllMatches();
+            }
+        }
+    }
+
+    //First for-loop counts null places and second shifts above sprite one down
+    private IEnumerator ShiftTilesDown(int x, int yStart, float shiftDelay = .03f)
+    {
+        IsShifting = true;
+        List<SpriteRenderer> renders = new List<SpriteRenderer>();
+        int nullCount = 0;
+
+        for(int y = yStart; y < ySize; y++)
+        {
+            SpriteRenderer render = tiles[x, y].GetComponent<SpriteRenderer>();
+
+            if(render.sprite == null)
+            {
+                nullCount++;
+            }
+
+            renders.Add(render);
+        }
+
+        for(int i = 0; i < nullCount; i++)
+        {
+            //Add to Score
+            GUIManager.instance.Score += 50;
+
+            yield return new WaitForSeconds(shiftDelay);
+            for(int k = 0; k < renders.Count - 1; k++)
+            {
+                renders[k].sprite = renders[k + 1].sprite;
+                renders[k + 1].sprite = GetNewSprite(x, ySize - 1);
+            }
+        }
+
+        IsShifting = false;
+    }
+
+    //refills the board
+    //the if statements removes sprites that may autocombo when filling board
+    private Sprite GetNewSprite(int x, int y)
+    {
+        List<Sprite> possibleCharacters = new List<Sprite>();
+        possibleCharacters.AddRange(characters);
+
+        if (x > 0)
+        {
+            possibleCharacters.Remove(tiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+
+        if (x < xSize - 1)
+        {
+            possibleCharacters.Remove(tiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+
+        if (y > 0)
+        {
+            possibleCharacters.Remove(tiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+        }
+
+        return possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+    }
+
+}
